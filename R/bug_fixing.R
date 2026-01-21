a29a6051d28c4b9058cce0a8c092f59d212f4c66
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2026-01-21 07:36:36 +0100

    Delete R/bug_fixing.R

--- R/bug_fixing.R
+++
@@ -1,6 +0,0 @@
-
-input_path <- "Q:/20-forskning/20-dfad/data/Data/additional_data/SKAW/additional_mac_samples/skaw/"
-file <- "LK 429 Altaire Bifangst 07.11.2025.xlsx"
-# input_path <- paste0(dir_path_all, "/", file)
-
-output_path <- "C:/Users/kibi/OneDrive - Danmarks Tekniske Universitet/test"
